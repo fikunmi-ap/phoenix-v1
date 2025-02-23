fee75bf32298355713c8ab80dce74fe0270a6a9f
fikunmi-ap <ajayipetersoluwafikunmi@gmail.com>
2025-02-23 02:13:46 +0100

    Changed IDD and log authority

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ security_txt! {
     auditors: "contact@osec.io"
 }
 
-declare_id!("PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY");
+declare_id!("8iDneAPbCtYsVGVb7ucnNMqxmJjpEazY3jKT3n56MoSY");
 
 /// This is a static PDA with seeds: [b"log"]
 /// If the program id changes, this will also need to be updated
@@ -67,8 +67,8 @@ pub mod phoenix_log_authority {
     // The address of the PDA is 7aDTsspkQNGKmrexAN7FLx9oxU3iPczSSvHNggyuqYkR
     // The bump seed is stored in a variable called bump()
     declare_pda!(
-        "7aDTsspkQNGKmrexAN7FLx9oxU3iPczSSvHNggyuqYkR",
-        "PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY",
+        "6zFwG3KozYkxy5Jt73Zy1cm83P6iz2SycmyqokaFBPAG",
+        "8iDneAPbCtYsVGVb7ucnNMqxmJjpEazY3jKT3n56MoSY",
         "log"
     );
 
